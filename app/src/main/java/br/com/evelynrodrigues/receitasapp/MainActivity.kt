8be60c81f7c2afa4d9678a376008eb6b5931ce3e
Évelyn Rodrigues <rodrigueseconsultoria@gmail.com>
2026-03-22 06:50:09 -0300

    Enhance MainActivity and update AndroidManifest for network access
    - Added INTERNET permission and enabled cleartext traffic in AndroidManifest.xml.
    - Refactored MainActivity to utilize ReceitaViewModel for managing recipe data.
    - Replaced the previous greeting UI with ReceitaListScreen for displaying recipes.

--- app/src/main/java/br/com/evelynrodrigues/receitasapp/MainActivity.kt
+++ app/src/main/java/br/com/evelynrodrigues/receitasapp/MainActivity.kt
@@ -4,44 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import br.com.evelynrodrigues.receitasapp.data.local.database.AppDatabase
+import br.com.evelynrodrigues.receitasapp.data.remote.api.RetrofitClient
+import br.com.evelynrodrigues.receitasapp.data.repository.ReceitaRepository
+import br.com.evelynrodrigues.receitasapp.ui.receita.ReceitaListScreen
+import br.com.evelynrodrigues.receitasapp.ui.receita.ReceitaViewModel
+import br.com.evelynrodrigues.receitasapp.ui.receita.ReceitaViewModelFactory
 import br.com.evelynrodrigues.receitasapp.ui.theme.ReceitasAppTheme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+
+    private val api by lazy {
+        RetrofitClient().getReceitaApi()
+    }
+
+    private val dao by lazy {
+        AppDatabase.getDatabase(applicationContext).receitaDao()
+    }
+
+    private val repository by lazy {
+        ReceitaRepository(api, dao)
+    }
+
+    private val viewModel: ReceitaViewModel by viewModels {
+        ReceitaViewModelFactory(repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ReceitasAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ReceitaListScreen(viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReceitasAppTheme {
-        Greeting("Android")
     }
 }
